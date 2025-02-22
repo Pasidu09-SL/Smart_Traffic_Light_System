486c39175b5e83a05015c82e26fb46947712f842
Pasidu Sachintha <m.k.p.sachintha09@gmail.com>
2025-02-22 19:53:57 +0530

    Fully updated timing traffic light system

--- Traffic_Light_System_2.0/Traffic_Light_System_2.0.ino
+++ Traffic_Light_System_2.0/Traffic_Light_System_2.0.ino
@@ -31,6 +31,22 @@ TrafficLight  westL = {44, 45, 46},
 const int PIR_1 = A2, PIR_2 = A9; //North (A2 - A8)   East(A9 - A15)
 const int ULTRA_TRIGGER[] = {A3, A5, A7, A10, A12, A14};
 const int ULTRA_ECHO[] = {A4, A6, A8, A11, A13, A15};
+//North PIR Senseor - A2
+//North Left lane Ultra Sonic Sensor Trigger - A3
+//North Left lane Ultra Sonic Sensor Echo - A4
+//North Middle lane Ultra Sonic Sensor Trigger - A5
+//North Middle lane Ultra Sonic Sensor Echo - A6
+//North Right lane Ultra Sonic Sensor Trigger - A7
+//North Right lane Ultra Sonic Sensor Echo - A8
+
+//East PIR Senseor - A9
+//East Left lane Ultra Sonic Sensor Trigger - A10
+//East Left lane Ultra Sonic Sensor Echo - A11
+//East Middle lane Ultra Sonic Sensor Trigger - A12
+//East Middle lane Ultra Sonic Sensor Echo - A13
+//East Right lane Ultra Sonic Sensor Trigger - A14
+//East Right lane Ultra Sonic Sensor Echo - A15
+
 
 // Timing constants (milliseconds)
 const int greenTime = 20000, yellowTime = 3000, rightTurnTime = 10000;
@@ -58,8 +74,22 @@ void switchToGreen(TrafficLight light){
 void setAllRed(){
   TrafficLight lights[] = {northL, northM, northR, eastL, eastM, eastR, southL, southM, southR, westL, westM, westR};
   for (int i = 0; i < sizeof(lights) / sizeof(lights[0]); i++) {
-        setTrafficLight(lights[i], RED);  
+    // Check if the current light is green
+    if (digitalRead(lights[i].green) == HIGH) {
+    // Transition: Green → Yellow → Red
+    setTrafficLight(lights[i], YELLOW); // Set to yellow
+    delay(yellowTime); // Wait for yellow time
+    setTrafficLight(lights[i], RED); // Set to red
+    } else {
+      // If not green, set directly to red
+      setTrafficLight(lights[i], RED);
+    }
   }
+  digitalWrite(p1Green,LOW);
+  digitalWrite(p2Green,LOW);
+  digitalWrite(p3Green,LOW);
+  digitalWrite(p4Green,LOW);
+
   digitalWrite(p1Red,HIGH);
   digitalWrite(p2Red,HIGH);
   digitalWrite(p3Red,HIGH);
@@ -116,7 +146,7 @@ void phase5_PedestrianAllDirection() {
   digitalWrite(p2Red,LOW);
   digitalWrite(p3Red,LOW);
   digitalWrite(p4Red,LOW);
-  
+
   delay(safetyDelay);
 
   digitalWrite(p1Green,HIGH);
